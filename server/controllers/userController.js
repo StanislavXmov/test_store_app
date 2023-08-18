5192878891a557eef042cd0c72b4954a7ecc00c7
stanislav <xiiiovart@gmail.com>
2023-08-18 21:52:11 +0300

    add user

---
+++ server/controllers/userController.js
@@ -0,0 +1,58 @@
+const uuid = require('uuid');
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const ApiError = require('../error/apiError');
+const { User } = require('../models/user');
+
+const generateJwt = (id, nickname, role) => {
+  return jwt.sign(
+    {id, nickname, role},
+    process.env.SECRET_KEY,
+    {expiresIn: '24h'}
+  );
+}
+
+class UserController {
+  async registration (req, res, next) {
+    const { nickname, password, role } = req.body;
+    console.log(nickname, password, role);
+    
+    if (!nickname || !password) {
+      return next(ApiError.badRequest('invalid nickname or password'));
+    }
+    const candidate = await User.findOne({where: {nickname}});
+    if (candidate) {
+      return next(ApiError.badRequest('user with this nickname already exists'));
+    }
+    const {avatar} = req.files;
+    let fileName = uuid.v4() + '.jpg';
+    avatar.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({nickname, role, password: hashPassword, avatar: fileName,});
+    const token = generateJwt(user.id, nickname, user.role);
+    return res.json({token});
+  }
+
+  async login (req, res, next) {
+    const { nickname, password } = req.body;
+    const user = await User.findOne({where: {nickname}});
+    if (!user) {
+      return next(ApiError.internalRequest('user not found'));
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internalRequest('user not found'));
+    }
+    const token = generateJwt(user.id, user.nickname, user.role);
+    return res.json({token});
+  }
+
+  async check (req, res) {
+    const token = generateJwt(req.user.id, req.user.nickname, req.user.role);
+    return res.json({token});
+  }
+}
+
+module.exports = new UserController();
--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const sequelize = require('./db');
+const models = require('./models/user');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/errorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +15,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
+app.use('/api', router);
+
+app.use(errorHandler);
 
 const start = async () => {
   try {
