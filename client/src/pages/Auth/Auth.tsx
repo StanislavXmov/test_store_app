61be2432a2ef34200169e6b846310d503a670ea0
stanislav <xiiiovart@gmail.com>
2023-08-20 23:02:00 +0300

    add form validation

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -7,16 +7,33 @@ import styles from './Auth.module.scss';
 interface AuthForm {
   nickname: string;
   password: string;
+  avatar: FileList;
+}
+
+enum ErrorType {
+  required = 'required',
+  minLength = 'minLength',
+}
+
+const errorMessage = {
+  [ErrorType.required]: (name: string) => `${name} is required`,
+  [ErrorType.minLength]: (length: number) => `min length ${length}`,
 }
 
 export const Auth = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const { register, handleSubmit } = useForm<AuthForm>({
+  const { 
+    register, 
+    handleSubmit, 
+    watch,
+    formState: { errors },
+  } = useForm<AuthForm>({
     defaultValues: {
       nickname: '',
       password: '',
-    }
+    },
+    mode: 'onChange',
   });
   const isLogin = location.pathname === getRouteLogin();
 
@@ -30,13 +47,55 @@ export const Auth = () => {
       <form className={styles.formWrapper} onSubmit={handleSubmit(onSubmit)}>
       <label className={styles.formLabel}>
         nickname:
-        <input type='text' className={styles.formInput} {...register('nickname')} />
+        <input 
+          type='text' 
+          className={styles.formInput} 
+          placeholder='enter nickname'
+          {...register('nickname', {required: true, minLength: 3})} 
+        />
+        <span className={styles.errorMessage}>
+          {errors.nickname?.type === ErrorType.required && errorMessage[ErrorType.required]('nickname')}
+          {errors.nickname?.type === ErrorType.minLength && errorMessage[ErrorType.minLength](3)}
+        </span>
       </label>
       <label className={styles.formLabel}>
         password:
-        <input type='password' className={styles.formInput} {...register('password')} />
+        <input 
+          type='password' 
+          className={styles.formInput}
+          placeholder='enter password'
+          {...register('password', {required: 'password is required', minLength: 5})} 
+        />
+        <span className={styles.errorMessage}>
+          {errors.password?.type === ErrorType.required && errorMessage[ErrorType.required]('password')}
+          {errors.password?.type === ErrorType.minLength && errorMessage[ErrorType.minLength](5)}
+        </span>
       </label>
-      <button className={styles.submitButton} type="submit">SUBMIT</button>
+      <label className={styles.formLabel}>
+        avatar:
+        <input 
+          type='file' 
+          className={styles.hide} 
+          {...register('avatar', {required: 'avatar is required'})} 
+        />
+        <div className={`${styles.formInput} ${styles.lineHeight}`} >
+          {watch('avatar')?.[0]?.name
+            ? (<span>{watch('avatar')[0].name}</span>)
+            : (<span className={styles.fileName} >avatar.png</span>)
+          }
+        </div>
+        <span className={styles.errorMessage}>
+          {errors.avatar?.type === ErrorType.required && errorMessage[ErrorType.required]('avatar')}
+        </span>
+      </label>
+      <button 
+        className={styles.submitButton} 
+        type="submit"
+      >
+        SUBMIT
+      </button>
+      {/* TEST ERRORS */}
+      {/* <button className={styles.submitButton} type="button" onClick={() => console.log(errors)}>SUBMIT</button> */}
     </form>
     </div>
   );
